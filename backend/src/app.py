af9b03cbab572b46f25955403163c39e69a1a162
Alex <foulonalex@yahoo.com>
2024-05-30 19:17:51 +0200

    Database model + Rebuild endpoint

--- backend/src/app.py
+++ backend/src/app.py
@@ -2,12 +2,14 @@ from flask import Flask
 from config import Config
 from routes.quiz import quiz_bp
 from routes.auth import auth_bp
+from routes.db import db_bp
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 app.register_blueprint(quiz_bp)
 app.register_blueprint(auth_bp)
+app.register_blueprint(db_bp)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ backend/src/routes/db.py
@@ -0,0 +1,56 @@
+from flask import Blueprint, request, jsonify
+from db import get_db
+import datetime
+import utils.jwt_utils
+
+db_bp = Blueprint('database', __name__)
+
+@db_bp.route('/rebuild-db', methods=['POST'])
+def rebuild_db():
+    db = get_db()
+    cursor = db.cursor()
+
+    cursor.execute("DROP TABLE IF EXISTS questions")
+    cursor.execute("DROP TABLE IF EXISTS answers")
+    cursor.execute("DROP TABLE IF EXISTS participations")
+    cursor.execute("DROP TABLE IF EXISTS participation_answers")
+
+    cursor.execute("""
+    CREATE TABLE questions (
+        question_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        question_text TEXT NOT NULL,
+        image_url TEXT,
+        position INTEGER NOT NULL UNIQUE
+    )
+    """)
+    cursor.execute("""
+    CREATE TABLE answers (
+        answer_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        question_id INTEGER NOT NULL,
+        answer_text TEXT NOT NULL,
+        is_correct BOOLEAN NOT NULL,
+        FOREIGN KEY (question_id) REFERENCES questions (question_id)
+    )
+    """)
+    cursor.execute("""
+    CREATE TABLE participations (
+        participation_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT NOT NULL,
+        score INTEGER NOT NULL,
+        timestamp DATETIME NOT NULL
+    )
+    """)
+    cursor.execute("""
+    CREATE TABLE participation_answers (
+        participation_answer_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        participation_id INTEGER NOT NULL,
+        question_id INTEGER NOT NULL,
+        answer_id INTEGER NOT NULL,
+        FOREIGN KEY (participation_id) REFERENCES participations (participation_id),
+        FOREIGN KEY (question_id) REFERENCES questions (question_id),
+        FOREIGN KEY (answer_id) REFERENCES answers (answer_id)
+    )
+    """)
+    db.commit()
+
+    return "Ok", 200
