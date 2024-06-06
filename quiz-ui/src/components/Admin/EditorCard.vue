c99af7aa7207b2429bfdf52d0e4943705510366e
erwan44gaut <erwan.gautier@edu.esiee.fr>
2024-06-06 12:09:51 +0200

    add loger

---
+++ quiz-ui/src/components/Admin/EditorCard.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+defineProps({
+	quizzList: {
+		type: Array as PropType<Quizz[]>,
+		required: true
+	}
+});
+</script>
+
+<template>
+	<div>
+		
+	</div>
+</template>
+
+<style scoped>
+
+</style>
--- quiz-ui/src/components/Admin/LoginForm.vue
+++ quiz-ui/src/components/Admin/LoginForm.vue
@@ -1,37 +1,57 @@
 <script setup lang="ts">
 import { ref } from 'vue';
+import { useRouter } from 'vue-router';
 
-const visible = ref(false);
-const value = ref('');
+const visible = ref(true);
+const password = ref('');
+const correctPassword = 'test';
+const router = useRouter();
+let flag: boolean = false;
+
+const validatePassword = () => {
+	if (password.value == correctPassword) {
+		visible.value = false;
+		flag = true;
+		router.push({
+			name: 'admin'
+		});
+	} else {
+		alert('Incorrect password');
+		router.push({
+			name: 'home'
+		});
+	}
+};
+
+const handleDialogClose = () => {
+	if (!flag) router.push('/');
+};
 </script>
 
 <template>
-    <div class="card">
-        <VueButton label="Connection" @click="visible = true" />
-        <VueDialog v-model:visible="visible" modal header="Admin Connection">
-            <div class="message">
-                <span>Enter your password.</span>
-            </div>
-            <div class="bottom">
-                <VuePassword id="input" v-model="value" toggleMask :feedback="false" placeholder="******"/>
-                <VueButton id="button" type="button" label="Connection" @click="visible = false"></VueButton>
-            </div>
-        </VueDialog>
+  <VueDialog v-model:visible="visible" modal header="Admin Connection" @hide="handleDialogClose">
+    <div class="message">
+      <span>Enter your password.</span>
     </div>
+    <div class="bottom">
+      <VuePassword id="input" v-model="password" toggleMask :feedback="false" placeholder="******" />
+      <VueButton id="button" type="button" label="Connection" @click="validatePassword"></VueButton>
+    </div>
+  </VueDialog>
 </template>
 
 <style scoped>
 .bottom {
-    display: flex;
-    gap: 10px;
-    width: 100%;
+  display: flex;
+  gap: 10px;
+  width: 100%;
 }
 
 .message {
-    margin-bottom: 10px;
+  margin-bottom: 10px;
 }
 
 #input {
-    flex: 1;
+  flex: 1;
 }
 </style>
