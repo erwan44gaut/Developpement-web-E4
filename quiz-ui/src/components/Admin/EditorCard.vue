1c9f9e975282484c39b68b580dcbba4f79015b92
erwan44gaut <erwan.gautier@edu.esiee.fr>
2024-06-06 12:20:30 +0200

    fix

--- quiz-ui/src/components/Admin/EditorCard.vue
+++ quiz-ui/src/components/Admin/EditorCard.vue
@@ -1,16 +1,10 @@
 <script setup lang="ts">
 
-defineProps({
-	quizzList: {
-		type: Array as PropType<Quizz[]>,
-		required: true
-	}
-});
 </script>
 
 <template>
 	<div>
-		
+
 	</div>
 </template>
 
