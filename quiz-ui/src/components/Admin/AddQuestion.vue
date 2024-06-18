ce0f76215aa998313e970a43b0de642575cda5de
Alex <foulonalex@yahoo.com>
2024-06-18 20:55:56 +0200

    Change Add question design

--- quiz-ui/src/components/Admin/AddQuestion.vue
+++ quiz-ui/src/components/Admin/AddQuestion.vue
@@ -47,6 +47,10 @@ const removeAnswer = (answer_id: number) => {
 	}
 };
 
+const setRightAnswer = (answer_id: number) => {
+	correctAnswerId.value = answer_id;
+};
+
 const saveQuestion = async () => {
 	if (newQuestion.value.title === '') {
 		alert('Title cannot be empty');
@@ -105,41 +109,42 @@ onMounted(quizInfo);
   <div>
     <div class="add nes-btn is-success" @click="dialogVisible = true">
       <span class="add-text">Add new question</span>
-      <i class="pi pi-plus"></i>
     </div>
 
-	<Dialog :visible="dialogVisible" modal class="nes-container is-dark" headerless>
-		<template #container="{ closeCallback }">
-			<div class="dialog-content">
-				<label for="title">Title</label>
-				<VueInputText class="nes-input is-dark" id="title" v-model="newQuestion.title"/>
+	<div :visible="dialogVisible" modal headerless class="dialog">
+		<div class="nes-container is-dark">
+			<label for="title">Title</label>
+			<VueInputText class="nes-input is-dark" id="title" v-model="newQuestion.title"/>
 
-				<label for="text">Text</label>
-				<VueInputText class="nes-input is-dark" id="text" v-model="newQuestion.text"/>
+			<label for="text">Text</label>
+			<VueInputText class="nes-input is-dark" id="text" v-model="newQuestion.text"/>
 
-				<label for="image">Image URL</label>
-				<VueInputText class="nes-input is-dark" id="image" v-model="newQuestion.image"/>
+			<label for="image">Image URL</label>
+			<VueInputText class="nes-input is-dark" id="image" v-model="newQuestion.image"/>
 
-				<label for="position">Position</label>
-				<VueInputText class="nes-input is-dark" id="position" v-model="newQuestion.position" disabled/>
-
-        		<VueButton label="Add Answer" @click="addAnswer" class="nes-btn is-success" />
-				<p class="info">Click on one of the radio buttons, to select the right answer.</p>
-				<div v-for="(answer, index) in newQuestion.possibleAnswers" :key="answer.answer_id">
-					<label :for="'answer-' + index">Answer {{ index + 1 }}</label>
-					<div class="answer-input">
-						<VueInputText :id="'answer-' + index" v-model="answer.text" class="nes-input is-dark answer-text"/>
-						<VueRadioButton :value="answer.answer_id" v-model="correctAnswerId" />
-						<i class="pi pi-trash" @click="removeAnswer(answer.answer_id)"></i>
-					</div>
-				</div>
-				<div class="footer-buttons">
-					<VueButton label="Cancel" icon="pi pi-times" class="nes-btn is-error" severity="danger" @click="dialogVisible = false" />
-					<VueButton label="Save" icon="pi pi-check" class="nes-btn is-success" @click="saveQuestion" />
+			<label for="position">Position</label>
+			<VueInputText class="nes-input is-dark" id="position" v-model="newQuestion.position" disabled/>
+			
+			<br><br><br>
+			<h1 class="">Answers</h1>
+			<p class="info nes-text is-primary">Select the right answer using the blue buttons.</p>
+			<VueButton label="+ Add Answer" @click="addAnswer" class="nes-btn is-primary" style="width: 100%; text-align: left;"/>
+			<br><br>
+			<div v-for="(answer, index) in newQuestion.possibleAnswers" :key="answer.answer_id">
+				<label :for="'answer-' + index">Answer {{ index + 1 }}</label>
+				<div class="answer-input">
+					<VueInputText :id="'answer-' + index" v-model="answer.text" class="nes-input is-dark answer-text"/>
+					<button style="margin-left: 1rem;" :disabled="answer.answer_id == correctAnswerId" :class="['nes-btn is-primary', {'is-disabled' : answer.answer_id == correctAnswerId}]" @click="setRightAnswer(answer.answer_id)">Set as true</button>
+					<button style="margin-left: 0.5rem;" class="nes-btn is-error" @click="removeAnswer(answer.answer_id)">Remove</button>
 				</div>
 			</div>
-		</template>
-	</Dialog>
+			<br>
+			<div class="footer-buttons">
+				<VueButton label="Cancel" class="nes-btn is-error" severity="danger" @click="dialogVisible = false" />
+				<VueButton label="Save" class="nes-btn is-success" @click="saveQuestion" />
+			</div>
+		</div>
+	</div>
   </div>
 </template>
 
@@ -147,6 +152,23 @@ onMounted(quizInfo);
 .add {
   width: 100%;
   height: 100%;
+}
+
+.dialog
+{
+	padding: 0;
+	margin: 0;
+	position: fixed;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	background-color: rgba(0, 0, 0, 0.5);
+	overflow: auto;
+}
+
+.nes-container {
+	  margin: 10%;
 }
 
 .answer-input
@@ -160,5 +182,9 @@ onMounted(quizInfo);
 .answer-text
 {
 	widows: 70%;
+}
+
+.nes-btn {
+	white-space: nowrap;
 }
 </style>
