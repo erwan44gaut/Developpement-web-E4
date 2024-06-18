612176d400e0f70f2722f7c1c5088e626f58586c
Alex <foulonalex@yahoo.com>
2024-06-18 10:21:13 +0200

    rename play

--- quiz-ui/src/App.vue
+++ quiz-ui/src/App.vue
@@ -16,7 +16,7 @@ const playRefresh = () => {
 					<svg class="nav-icon" width="30" height="30" fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
 						<path d="M2 5h20v14H2V5zm18 12V7H4v10h16zM8 9h2v2h2v2h-2v2H8v-2H6v-2h2V9zm6 0h2v2h-2V9zm4 4h-2v2h2v-2z" fill="black"/>
 					</svg>
-					<RouterLink to="/play" @click.prevent="playRefresh()">Jouer</RouterLink>
+					<RouterLink to="/play" @click.prevent="playRefresh()">Accueil</RouterLink>
 				</div>
 				<div class="nav-item">
 					<svg class="nav-icon" width="30" height="30" fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
