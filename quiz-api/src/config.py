44aeaed7698d1028ce6fe29af2f5c4a600bfdd75
Alex <foulonalex@yahoo.com>
2024-06-18 21:18:00 +0200

    Update password

--- quiz-api/src/config.py
+++ quiz-api/src/config.py
@@ -2,5 +2,5 @@ import hashlib
 
 class Config:
     DATABASE = '../db/database.db'
-    SECRET_KEY = 'test'
+    SECRET_KEY = 'flask2023'
     SECRET_KEY_HASH = hashlib.md5(SECRET_KEY.encode()).hexdigest()
